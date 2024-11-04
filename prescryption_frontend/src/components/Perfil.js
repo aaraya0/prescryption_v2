1f7eb13dab888fc2d2be7eb769e93f4b987d8aa7
belenaguilarv <belenaguiilarv@gmail.com>
2024-11-03 22:38:33 -0300

    update menú doctor

--- prescryption_frontend/src/components/Perfil.js
+++ prescryption_frontend/src/components/Perfil.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './styles.css'; 
 
 function Perfil() {
     const [profile, setProfile] = useState(null);
@@ -24,43 +25,48 @@ function Perfil() {
     if (error) return <p>{error}</p>;
 
     return (
-        <div>
+        <div className="perfil-container">
             <h2>Perfil</h2>
             {profile ? (
-                <div>
-                    <p><strong>Tipo de Usuario:</strong> {profile.userType}</p>
-                    <p><strong>Nombre y Apellido:</strong> {profile.profile.name} {profile.profile.surname}</p>
-                    {profile.userType === 'patient' && (
-                        <>
-                            <p><strong>DNI:</strong> {profile.profile.nid}</p>
-                            <p><strong>Fecha de Nacimiento:</strong> {profile.profile.birth_date}</p>
-                            <p><strong>Sexo:</strong> {profile.profile.sex}</p>
-                            <p><strong>Obra Social:</strong> {profile.profile.insurance_name}</p>
-                            <p><strong>Plan:</strong> {profile.profile.insurance_plan}</p>
-                        </>
-                    )}
-                    {profile.userType === 'doctor' && (
-                        
-                        <>  <p><strong>DNI:</strong> {profile.profile.nid}</p>
-                            <p><strong>Especialidad:</strong> {profile.profile.specialty}</p>
-                            <p><strong>Licencia:</strong> {profile.profile.license}</p>
-                        </>
-                    )}
-                    {profile.userType === 'pharmacy' && (
-                        <>
-                            <p><strong>CUIT:</strong> {profile.profile.pharmacy_nid}</p>
-                            <p><strong>Nombre de la Farmacia:</strong> {profile.profile.pharmacy_name}</p>
-                            <p><strong>Número de Farmacia:</strong> {profile.profile.pharmacy_nid}</p>
-                            <p><strong>Alias de Farmacia:</strong> {profile.profile.alias}</p>
-                        </>
-                    )}
-                    {profile.userType === 'insurance' && (
-                        <>
-                            <p><strong>CUIT:</strong> {profile.profile.insurance_nid}</p>
-                            <p><strong>Nombre de la Compañía:</strong> {profile.profile.insurance_name}</p>
-                            <p><strong>ID de la Compañía:</strong> {profile.profile.insurance_nid}</p>
-                        </>
-                    )}
+                <div className="perfil-card">
+                    <div className="perfil-image">
+                        <img src={profile.profile.picture} alt="Profile image" />
+                    </div>
+                    <div className="perfil-info">
+                        <p><strong>Tipo de Usuario:</strong> {profile.userType}</p>
+                        <p><strong>Nombre y Apellido:</strong> {profile.profile.name} {profile.profile.surname}</p>
+                        {profile.userType === 'patient' && (
+                            <>
+                                <p><strong>DNI:</strong> {profile.profile.nid}</p>
+                                <p><strong>Fecha de Nacimiento:</strong> {profile.profile.birth_date}</p>
+                                <p><strong>Sexo:</strong> {profile.profile.sex}</p>
+                                <p><strong>Obra Social:</strong> {profile.profile.insurance_name}</p>
+                                <p><strong>Plan:</strong> {profile.profile.insurance_plan}</p>
+                            </>
+                        )}
+                        {profile.userType === 'doctor' && (
+                            <>
+                                <p><strong>DNI:</strong> {profile.profile.nid}</p>
+                                <p><strong>Especialidad:</strong> {profile.profile.specialty}</p>
+                                <p><strong>Licencia:</strong> {profile.profile.license}</p>
+                            </>
+                        )}
+                        {profile.userType === 'pharmacy' && (
+                            <>
+                                <p><strong>CUIT:</strong> {profile.profile.pharmacy_nid}</p>
+                                <p><strong>Nombre de la Farmacia:</strong> {profile.profile.pharmacy_name}</p>
+                                <p><strong>Número de Farmacia:</strong> {profile.profile.pharmacy_nid}</p>
+                                <p><strong>Alias de Farmacia:</strong> {profile.profile.alias}</p>
+                            </>
+                        )}
+                        {profile.userType === 'insurance' && (
+                            <>
+                                <p><strong>CUIT:</strong> {profile.profile.insurance_nid}</p>
+                                <p><strong>Nombre de la Compañía:</strong> {profile.profile.insurance_name}</p>
+                                <p><strong>ID de la Compañía:</strong> {profile.profile.insurance_nid}</p>
+                            </>
+                        )}
+                    </div>
                 </div>
             ) : (
                 <p>Cargando información del perfil...</p>
