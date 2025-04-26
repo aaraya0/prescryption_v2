ccb04e04d3e03b871006bdbc7df80e076369dcca
belenaguilarv <belenaguiilarv@gmail.com>
2025-04-26 17:36:38 -0300

    Interfaz que redirecciona al tipo de farmaceútic@

---
+++ prescryption_frontend/src/components/PharmacyType.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles.css';
+
+
+function PharmacyType() {
+    const navigate = useNavigate();
+
+    const handleSelection = (tipo) => {
+        document.cookie = `userType=${tipo}`;
+        navigate('/login');
+    };
+
+    return (
+        <div className="formUserOptions">
+            <h2 className="loginTitle">Elegí el tipo de usuario</h2>
+            <div className="buttons">
+                <button className="patientButton" onClick={() => handleSelection('pharmacist')}>Usuario de Farmacia</button>
+                <button className="doctorsButton" onClick={() => handleSelection('pharmacyAdmin')}>Farmacia (Administrador)</button>
+            </div>
+        </div>
+    );
+}
+
+export default PharmacyType;
